bead9f7a4ffb2e252ba0dac364215e956ae65985
César Frias <cagfrias@gmail.com>
2010-09-15 23:15:38 -0300

    Adding tests

--- test_pomodoro_parser.py
+++ test_pomodoro_parser.py
@@ -9,6 +9,21 @@ class TestParse(TestCase):
         number = -5
         self.assertRaises(OptionValueError, check_positive_integer, number)
 
+    def test_raise_with_0(self):
+        number = 0
+        self.assertRaises(OptionValueError, check_positive_integer, number)
+
+    def test_returns_the_value_5(self):
+        number = 5
+        expected = 5
+        returned = check_positive_integer(number)
+        self.assertEqual(expected,  returned)
+
+    def test_returns_the_value_500(self):
+        number = 500
+        expected = 500
+        returned = check_positive_integer(number)
+        self.assertEqual(expected, returned)
 
 if __name__ == '__main__':
     main()
