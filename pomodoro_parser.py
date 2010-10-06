7abbdf4b83960282352861af0c9366aad01608c9
César <cagfrias@gmail.com>
2010-10-06 16:31:14 -0300

    Adiciona docstrings e corrige chamada de not_positive_integer no except

--- pomodoro_parser.py
+++ pomodoro_parser.py
@@ -1,23 +1,33 @@
 # -*- coding: utf-8 -*-
+"""
+Parses the options passed by command-line
+"""
 
 from copy import copy
 from optparse import Option, OptionParser, OptionValueError
 
 
 def check_positive_integer(options, option_used, value):
+    """ Verifies if the value passed is a number higher than zero """
     try:
         value = int(value)
         if value > 0:
             return value
         else:
-            not_positive_integer()
+            not_positive_integer(options, option_used)
     except OptionValueError:
-        not_positive_integer()
+        not_positive_integer(options, option_used)
 
-def not_positive_integer():
-    raise OptionValueError("Both arguments must be Positive Integers")
+def not_positive_integer(options, option_used):
+    """ 
+        Raises an OptionValueError if at least one of
+        the values is not higher than zero
+    """
+    raise OptionValueError("""Both arguments {0} must be Positive Integers.
+Option you have used {1}""".format(options, option_used))
 
 def option_parser():
+    """ Parses the option given by command-line """
     usage = "%prog [OPTIONS]"
     description = """
         %prog to better manage your time, as soon as your work time ends
@@ -48,6 +58,7 @@ def option_parser():
     return parser.parse_args()
 
 class PositiveInteger(Option):
+    """ Class used to verify if the values are higher than zero """
     TYPES = Option.TYPES + ("positiveInteger",)
     TYPE_CHECKER = copy(Option.TYPE_CHECKER)
     TYPE_CHECKER["positiveInteger"] = check_positive_integer
