452b47e13909c52f13ce3a9b7a258eb241ec29d3
César Frias <cagfrias@gmail.com>
2010-10-07 23:25:19 -0300

    Adding tests to the Timer class

--- test_pomodoro.py
+++ test_pomodoro.py
@@ -43,7 +43,7 @@ class TestParse(TestCase):
         )
 
 
-    def test_returns_the_value_5(self):
+    def test_returns_the_correct_value(self):
         """
             Test if check_positive_integer returns the correct number.
         """
@@ -51,18 +51,6 @@ class TestParse(TestCase):
         expected = 5
         returned = check_positive_integer('-w', '-r', number)
         self.assertEqual(expected,  returned)
-
-
-    def test_returns_the_value_500(self):
-        """
-            Test if check_positive_integer returns the correct number.
-        """
-        number = 500
-        expected = 500
-        returned = check_positive_integer('-w', '-r', number)
-        self.assertEqual(expected, returned)
-
-
 
 class TestUI(TestCase):
     """
@@ -76,14 +64,6 @@ class TestUI(TestCase):
         timer = Timer(300, 200)
         self.user_interface = UI(timer)
 
-    def test_seconds_to_minutes_1500(self):
-        """
-            Test if returns 25 minutes and zero seconds.
-        """
-        time_left = 1500
-        returned = self.user_interface.seconds_to_minutes(time_left)
-        self.assertEqual((25, 0), returned)
-
 
     def test_seconds_to_minutes_1000(self):
         """
@@ -94,15 +74,6 @@ class TestUI(TestCase):
         self.assertEqual((16, 40), returned)
 
 
-    def test_seconds_to_minutes_900(self):
-        """
-            Test if returns 15 minutes and zero seconds.
-        """
-        time_left = 900
-        returned = self.user_interface.seconds_to_minutes(time_left)
-        self.assertEqual((15, 0), returned)
-
-
     def test_seconds_to_minutes_005(self):
         """
             Test if returns zero minutes and 5 seconds.
@@ -110,6 +81,57 @@ class TestUI(TestCase):
         time_left = 005
         returned = self.user_interface.seconds_to_minutes(time_left)
         self.assertEqual((0, 5), returned)
+
+class TestTimer(TestCase):
+    """
+        Checks exclusively user interface.
+    """
+    def setUp(self):
+        """
+            Override the method setUp of TestCase.
+            I cannot take this name better :(
+        """
+        self.test_pomodoro = Timer()
+
+
+    def test_start(self):
+        """
+            When call the function start the expected value is running = True
+        """
+        self.assertFalse(self.test_pomodoro.running)
+        self.test_pomodoro.start()
+        self.assertTrue(self.test_pomodoro.running)
+
+
+    def test_pause(self):
+        """
+            When call the function pause the expected value is running = False
+        """
+        self.test_pomodoro.pause()
+        self.assertFalse(self.test_pomodoro.running)
+
+
+    def test_update_running(self):
+        """
+            When update is called with running = True the expected vale
+            is time_left - 1
+        """
+        self.test_pomodoro.time_left = 12
+        self.test_pomodoro.running = True
+        self.test_pomodoro.update()
+        self.assertEqual(self.test_pomodoro.time_left, 11)
+
+
+    def test_update_not_running(self):
+        """
+            When update is called with running = False the expected vale
+            is work_time
+        """
+        work_time = 1500
+        self.test_pomodoro.running = False
+        self.test_pomodoro.update()
+        self.assertEqual(self.test_pomodoro.time_left, work_time)
+
 
 
 if __name__ == '__main__':
