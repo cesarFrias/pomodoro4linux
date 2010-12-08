715888092df8928999f08973d5bcb1afc69fdc6a
César Frias <cagfrias@gmail.com>
2010-12-08 10:11:13 -0200

    Adiciona setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- encoding:utf-8 -*-
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='Pomodoro4linux',
+    version='1.0',
+    author='César Frias',
+    description = 'Tool for better manage your time',
+    license = 'GPL',
+    packages = ['pomodoro'],
+)
