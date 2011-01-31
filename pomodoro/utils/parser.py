c7f282b667f1d43c59b9f2f3c99da9222fc3ca6c
paivaNatasha <nattynpaiva@gmail.com>
2011-01-31 20:41:46 -0200

    Pep8 não faz mal

--- pomodoro/utils/parser.py
+++ pomodoro/utils/parser.py
@@ -18,20 +18,22 @@ def check_positive_integer(options, option_used, value):
     except OptionValueError:
         not_positive_integer(options, option_used)
 
+
 def not_positive_integer(options, option_used):
-    """ 
+    """
         Raises an OptionValueError if at least one of
         the values is not higher than zero
     """
     raise OptionValueError("""Both arguments %s must be Positive Integers.
 Option you have used %s""" % (options, option_used))
 
+
 def option_parser():
     """ Parses the option given by command-line """
     usage = "%prog [OPTIONS]"
     description = """
         %prog to better manage your time, as soon as your work time ends
-        starts your rest time. 
+        starts your rest time.
     """
 
     parser = OptionParser(usage, description=description,
@@ -39,23 +41,24 @@ def option_parser():
     parser.add_option(
         '-w',
         '--work',
-        action = 'store',
-        type = 'positiveInteger',
-        dest = 'work_time',
-        help = 'Define the time of each work round',
-        default = 1500
+        action='store',
+        type='positiveInteger',
+        dest='work_time',
+        help='Define the time of each work round',
+        default=1500
     )
 
     parser.add_option(
         '-r',
         '--rest',
-        action = 'store',
-        type = 'positiveInteger',
-        dest = 'rest_time',
-        help = 'Define the time of each rest round',
-        default = 300
+        action='store',
+        type='positiveInteger',
+        dest='rest_time',
+        help='Define the time of each rest round',
+        default=300
     )
     return parser.parse_args()
+
 
 class PositiveInteger(Option):
     """ Class used to verify if the values are higher than zero """
