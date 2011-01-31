dea6dba4aa7185972627e939b9831d2367728698
paivaNatasha <nattynpaiva@gmail.com>
2011-01-31 20:41:55 -0200

    Pep8 não faz mal

--- pomodoro/utils/utils.py
+++ pomodoro/utils/utils.py
@@ -4,6 +4,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+
 def seconds_to_minutes(time_left):
     """
         Transforms seconds in minutes.
@@ -13,9 +14,10 @@ def seconds_to_minutes(time_left):
 
     return minutes_left, seconds_left
 
+
 def show_menu(status_icon, button, time, data, widget=None):
     """
-        This method is just for display the menu 
+        This method is just for display the menu
     """
     data.show_all()
     data.popup(None, None, widget, button, time, status_icon)
